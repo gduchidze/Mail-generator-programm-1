138d2d330f0ec9b66cdd8ae3a083b80c25951fad
Giorgi Duchidze <124621555+gduchidze@users.noreply.github.com>
2024-02-04 20:31:15 +0400

    saboloo

--- OOP Properity.py
+++ OOP Properity.py
@@ -1,10 +1,9 @@
-# mail generator program
-
 class Student:
     def __init__(self, name, lastname, university_nick):
         self.name = name
         self._lastname = lastname
         self.__university = university_nick
+
     @property
     def fullname(self):
         return '{} {}'.format(self.name, self._lastname)
@@ -17,9 +16,24 @@ class Student:
 
     @property
     def email(self):
-        return f'{self.name}.{self._lastname}@{self.__university}.edu.ge'
+        return f'{self.name.lower()}.{self._lastname.lower()}@{self.__university.lower()}.edu.ge'
 
-# Example usage
+    def get_university(self):
+        return self.__university
+
+
+class Person(Student):
+    def __init__(self, name, lastname, university_nick, person_count):
+        super().__init__(name, lastname, university_nick)
+        self._person_count = person_count
+
+    @property
+    def email(self):
+        return f'{self.name.lower()}.{self._lastname.lower()}.{self._person_count}@{self.get_university().lower()}.edu.ge'
+
+
 st1 = Student('giorgi', 'duchidze', 'btu')
+st2 = Person('demetre', 'natidze', 'stu', 1)
 print(st1.email)
-
+print(st2.email)
+print(st1.get_university())
